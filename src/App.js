81a3294fdcdd654f5700cbcb4af0c3999db00de8
Amrik Singh Khalsa <singhamrikkhalsa@gmail.com>
2021-06-22 16:16:29 +0530

    grid added.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { Router, Route } from "react-router-dom";
+import { createBrowserHistory as createHistory } from "history";
+import HomePage from "./components/Homepage/Homepage";
 
 function App() {
+  const history = createHistory();
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router history={history}>
+        {/*         <TopBar />
+         */}{" "}
+        <Route path="/" exact component={HomePage} />
+        {/*         <Route path="/imagesearch" exact component={ImageSearchPage} />
+         */}{" "}
+      </Router>
     </div>
   );
 }
---
+++ src/components/Homepage/Homepage.jsx
@@ -0,0 +1,99 @@
+import React,{useState,useEffect} from 'react'
+import "./Homepage.css"
+import axios from "axios";
+function Homepage() {
+    const [imageData,setImageData] = useState([]);
+    const [isLoading,setIsLoading] = useState(true);
+    const [error,setError] = useState(null);
+    /* useEffect(async () => {
+        const user = await axios.get(
+          "https://reqres.in/api/users/"
+        )
+        let dataRec =  await user.json();
+        setImageData( dataRec)
+        console.log(imageData);
+        console.log("data",user.data.data) 
+       
+      }, []); */
+      const fetchData = async () => {
+        let res = await fetch(
+          "https://reqres.in/api/users/" 
+        );
+
+        let response = await res.json();
+        return response;
+        
+      };
+      useEffect(() => {
+        
+         fetchData()
+         .then((response)=>{
+         setIsLoading(false)
+         setImageData(response.data) // parse json
+         console.log(response.data)
+        })
+      }, []);
+    return (
+        <div >
+            
+            <main>
+            <section  className="cards">
+             {imageData&&imageData.map((image,index)=>(<>
+        <article>
+          <img key={index} className="article-img" src={image.avatar} alt=" " />
+          <h1 className="article-title">
+            {image.first_name} {image.last_name}
+            <br></br>
+            
+            {image.email} 
+         
+          </h1>
+          
+          
+        </article>
+        </>))} 
+        </section>
+       {/*  <section class="cards">
+        <article>
+          <img class="article-img" src="http://placekitten.com/305/205" alt=" " />
+          <h1 class="article-title">
+            Title of article
+          </h1>
+        </article>
+        <article>
+          <img class="article-img" src="http://placekitten.com/320/220" alt=" " />
+          <h1 class="article-title">
+            Title of article
+          </h1>
+        </article>
+        <article>
+          <img class="article-img" src="http://placekitten.com/330/240" alt=" " />
+          <h1 class="article-title">
+            Title of article
+          </h1>
+        </article>
+        <article>
+          <img class="article-img" src="http://placekitten.com/280/250" alt=" " />
+          <h1 class="article-title">
+            Title of article
+          </h1>
+        </article>
+        <article>
+          <img class="article-img" src="http://placekitten.com/310/210" alt=" " />
+          <h1 class="article-title">
+            Title of article
+          </h1>
+        </article>
+        <article>
+          <img class="article-img" src="http://placekitten.com/430/240" alt=" " />
+          <h1 class="article-title">
+            Title of article
+          </h1>
+        </article>
+      </section>
+        */} </main>
+        </div>
+    )
+}
+
+export default Homepage
