1985f67804d7aef8532bace459089adec205e7ee
Amrik Singh Khalsa <singhamrikkhalsa@gmail.com>
2021-06-23 17:08:12 +0530

    updates

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -5,17 +5,9 @@ function Navbar() {
  
       <header class="header-nav">
         <ul class="main-nav">
-          <li>
-            <a href="#">Home</a>
-          </li>
-          <li>
-            <a href="#">About</a>
-          </li>
-          <li>
-            <a href="#">Portfolio</a>
-          </li>
-          <li>
-            <a href="#">Contact</a>
+          
+          <li >
+            <a href="#">User List</a>
           </li>
         </ul>
       </header>
