32f5377f3d244b5ede07545a5bedff0ad6aa275a
Amrik Singh Khalsa <singhamrikkhalsa@gmail.com>
2021-06-23 17:56:44 +0530

    enzyme deps added.

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,10 @@
-import { render, screen } from '@testing-library/react';
+import {screen } from '@testing-library/react';
 import App from './App';
+import { shallow, mount, render } from 'enzyme';
 
 test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const wrapper = shallow(<App />);
+
+  console.log(wrapper)
+
 });
